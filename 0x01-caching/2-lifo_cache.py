4daaec2774bb744910fa80a916ca6fc09b4743cc
alli-cpp <alli-cpp@github.com>
2023-02-24 20:43:00 +0000

    lifo caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,35 +1,27 @@
 #!/usr/bin/python3
-"""
-Module 2-lifo_cache
-"""
+""" LIFO Caching """
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """
-    Overrides put() nad self()
-    from parent
-    """
-
+    """ Class that inherits from BaseCaching and is a caching system """
     def __init__(self):
-        """initialize"""
         super().__init__()
-        self.stack = []
+        self.last_key = ''
 
     def put(self, key, item):
-        """Add an item in the cache
-        """
+        """ Assign to the dictionary, LIFO algorithm, add element """
         if key and item:
-            if len(self.cache_data) == BaseCaching.MAX_ITEMS\
-                    and key not in self.stack:
-                del_key = self.stack.pop()
-                del self.cache_data[del_key]
-                print("DISCARD: {}".format(del_key))
             self.cache_data[key] = item
-            self.stack.append(key)
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                print("DISCARD: {}".format(self.last_key))
+                self.cache_data.pop(self.last_key)
+            self.last_key = key
 
     def get(self, key):
-        """Get an item by key
-        """
-        value = self.cache_data.get(key)
-        return 
+        """ Return the value linked """
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        if key in self.cache_data:
+            value = self.cache_data[key]
+            return value
